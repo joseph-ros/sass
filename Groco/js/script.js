54aab8cdc14bd5a4ecadf27a1aa56fb278fa24d2
Joseph Ros <rosjoseph.tran@gmail.com>
2025-08-26 13:33:04 +0700

    sass init

---
+++ Groco/js/script.js
@@ -0,0 +1,42 @@
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () => {
+    searchForm.classList.toggle('active');
+    cart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let cart = document.querySelector('.shopping-cart');
+
+document.querySelector('#cart-btn').onclick = () => {
+    cart.classList.toggle('active');
+    searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let loginForm = document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick = () => {
+    loginForm.classList.toggle('active');
+    cart.classList.remove('active');
+    searchForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+    loginForm.classList.remove('active');
+    cart.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+window.onscroll = () => {
+    navbar.classList.remove('active');
+    loginForm.classList.remove('active');
+    cart.classList.remove('active');
+    searchForm.classList.remove('active');
+}
---
+++ Groco/readme.md
@@ -0,0 +1,6 @@
+### Youtube Videos Here:
+
+```
+SCSS / SASS Web Design: https://www.youtube.com/watch?v=zTurVAVbPsY
+
+```
