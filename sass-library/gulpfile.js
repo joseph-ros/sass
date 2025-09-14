2555f4f0a1d4252abba6039bb11743b3c7b07074
Joseph Ros <rosjoseph.tran@gmail.com>
2025-09-14 12:57:39 +0700

    init

--- sass-library/gulpfile.js
+++ sass-library/gulpfile.js
@@ -2,13 +2,13 @@ const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'));
 
 function buildStyles() {
-    return src('*.scss')
+    return src('sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(dest('css'))
 }
 
 function watchTask() {
-    watch(['*.scss'], buildStyles)
+    watch(['sass/**/*.scss'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
